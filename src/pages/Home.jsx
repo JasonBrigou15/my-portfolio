1302452964270a3fbb018e034940e96f9ba3baec
Jason Brigou <jason.brigou@hotmail.com>
2026-02-17 17:09:52 +0100

    fixed the homepage not being the first thing the user sees. Also fixed the homepage for mobile users where text is now aligned centered

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,7 +16,7 @@ function Home() {
 
     return (
 <div className="min-h-screen bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${import.meta.env.BASE_URL}images/sunset-landscape.jpg)`, backgroundPosition: 'center 0%' }}>
-        <div className="flex flex-col items-center justify-center" style={{ height: 'calc(80vh - 80px)' }}> 
+        <div className="flex flex-col items-center justify-center text-center px-4" style={{ height: 'calc(80vh - 80px)' }}> 
             <h2 className="text-7xl font-bold text-white mb-4" style={textStyle}>HELLO WORLD!</h2>
             <h3 className="text-5xl font-bold text-white mb-2" style={textStyle}>I'M JASON BRIGOU</h3>
             <p className="text-2xl text-white font-semibold"style={textStyle}>Junior Full-Stack / .NET Developer</p>
